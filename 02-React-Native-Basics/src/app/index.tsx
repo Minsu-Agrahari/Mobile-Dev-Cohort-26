4428cad5132d273bee9843ee2ed316caae288f7b
Minsu-Agrahari <cmminsuagrahari@gmail.com>
2026-05-15 01:25:55 +0530

    Refactor HomeScreen component to use SafeAreaView and improve layout

--- 02-React-Native-Basics/src/app/index.tsx
+++ 02-React-Native-Basics/src/app/index.tsx
@@ -1,28 +1,45 @@
-import { StyleSheet, StatusBar, Text, View } from "react-native";
 import React from "react";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from 'expo-status-bar';
 
 const HomeScreen = () => {
-    const insets = useSafeAreaInsets();
-
-    console.log(insets);
-
     return (
-        <View
-            style={{
-                flex: 1,
-                paddingTop: insets.top,
-                paddingBottom: insets.bottom,
-                paddingLeft: insets.left,
-                paddingRight: insets.right,
-            }}
-        >
-            <StatusBar barStyle={"dark-content"} />
-            <Text>HomeScreen</Text>
-        </View>
+        <SafeAreaView>
+            <View style={styles.card}>
+                <StatusBar style="dark" />
+                <Text style={styles.title}>HomeScreen</Text>
+                <Text style={styles.subTitle}>Hello World !</Text>
+            </View>
+        </SafeAreaView>
     );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: "white",
+        borderRadius: 16,
+        padding: 20,
+        margin: 16,
+
+        // Android
+        elevation: 4,
+
+        // IOS
+        shadowColor: "#000",
+        shadowOpacity: 0.1,
+        shadowRadius: 8,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "#333",
+    },
+    subTitle: {
+        fontSize: 14,
+        color: "#888",
+        marginTop: 4,
+    },
+});
