077d5be24f4d8bd293560d98b7b20fd4539a262b
Minsu-Agrahari <cmminsuagrahari@gmail.com>
2026-05-14 23:53:08 +0530

    SafeAreaView

--- 02-React-Native-Basics/src/app/index.tsx
+++ 02-React-Native-Basics/src/app/index.tsx
@@ -1,13 +1,65 @@
-import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const HomeScreen = () => {
+function UnsafeScreen() {
     return (
-        <View>
-            <Text>HomeScreen</Text>
+        <View
+            style={{
+                flex: 1,
+                backgroundColor: "#1c1c1e",
+            }}
+        >
+            <Text
+                style={{
+                    color: "#fff",
+                    fontSize: 18,
+                    padding: 16,
+                }}
+            >
+                Header (bleeds under notch!)
+            </Text>
+
+            <Text style={{ color: "#aaa", padding: 16 }}>
+                This content might be hidden behind the status bar in dark mode.
+            </Text>
         </View>
+    );
+}
+
+function SafeScreen() {
+    return (
+        <SafeAreaView
+            style={{
+                flex: 1,
+                backgroundColor: "#1c1c1e",
+            }}
+        >
+            <Text
+                style={{
+                    color: "#fff",
+                    fontSize: 18,
+                    padding: 16,
+                }}
+            >
+                Header (safely below notch ... 🌹)
+            </Text>
+
+            <Text style={{ color: "#aaa", padding: 16 }}>
+                This content respects the safe area on all devices. 🤗
+            </Text>
+        </SafeAreaView>
+    );
+}
+const index = () => {
+    return (
+        <>
+            <SafeScreen />
+            {/* <UnsafeScreen /> */}
+        </>
     );
 };
 
-export default HomeScreen;
+export default index;
 
 const styles = StyleSheet.create({});
