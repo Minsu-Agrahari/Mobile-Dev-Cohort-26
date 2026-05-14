9673a7609c1d5390fff767db7470b228c4cbe7c8
Minsu-Agrahari <cmminsuagrahari@gmail.com>
2026-05-15 02:52:41 +0530

    feat: initialize React Native project with Expo, add basic layout and theming
    - Created package.json with dependencies and scripts for the project.
    - Added RootLayout component for navigation using expo-router.
    - Implemented HomeScreen component with theming support and basic structure.
    - Configured TypeScript with tsconfig.json for strict type checking and path aliases.

---
+++ Projects/02-React-Native-Basics/src/app/index.tsx
@@ -0,0 +1,172 @@
+// import React from "react";
+// import {
+//     Pressable,
+//     StyleSheet,
+//     Text,
+//     View,
+//     useWindowDimensions,
+// } from "react-native";
+// import * as ScreenOrientation from "expo-screen-orientation";
+// import { SafeAreaView } from "react-native-safe-area-context";
+
+// const HomeScreen = () => {
+//     const { height, width } = useWindowDimensions();
+
+//     const isTablet = width >= 768;
+//     const isLandscape = width > height;
+
+//     const lockLanscape = async () => {
+//         await ScreenOrientation.lockAsync(
+//             ScreenOrientation.OrientationLock.LANDSCAPE,
+//         );
+//     };
+
+//     const lockPortrait = async () => {
+//         await ScreenOrientation.lockAsync(
+//             ScreenOrientation.OrientationLock.PORTRAIT,
+//         );
+//     };
+
+//     return (
+//         <SafeAreaView style={{ flex: 1, padding: 16 }}>
+//             <Text
+//                 style={{
+//                     fontSize: width * 0.06,
+//                 }}
+//             >
+//                 Responsive Text 📱
+//             </Text>
+
+//             <View
+//                 style={{
+//                     flexDirection: isTablet ? "row" : "column",
+//                 }}
+//             >
+//                 <View
+//                     style={{
+//                         width: isTablet ? width / 2 : width - 32,
+//                         backgroundColor: "#6C63FF",
+//                         padding: 20,
+//                         borderRadius: 12,
+//                         marginBottom: isTablet ? 0 : 12,
+//                     }}
+//                 >
+//                     <Text style={{ color: "white" }}>Card 1</Text>
+//                 </View>
+
+//                 <View
+//                     style={{
+//                         width: isTablet ? width / 2 : width - 32,
+//                         backgroundColor: "#FF6584",
+//                         padding: 20,
+//                         borderRadius: 12,
+//                     }}
+//                 >
+//                     <Text
+//                         style={{
+//                             color: "white",
+//                         }}
+//                     >
+//                         Card 2
+//                     </Text>
+//                 </View>
+//             </View>
+
+//             <Text
+//                 style={{
+//                     color: "#888",
+//                     marginTop: 16,
+//                 }}
+//             >
+//                 Screen: {Math.round(width)} x {Math.round(height)}
+//                 {isLandscape ? "(Landscap)" : "(Portrait)"}
+//             </Text>
+
+//             {/* Orientation Buttons */}
+//             <View
+//                 style={{
+//                     flexDirection: "row",
+//                     gap: 12,
+//                     marginTop: 24,
+//                 }}
+//             >
+//                 <Pressable
+//                     onPress={lockLanscape}
+//                     style={{
+//                         flex: 1,
+//                         backgroundColor: "#6C63FF",
+//                         padding: 12,
+//                         borderRadius: 8,
+//                         alignItems: "center",
+//                     }}
+//                 >
+//                     <Text
+//                         style={{
+//                             color: "white",
+//                         }}
+//                     >
+//                         Force Landscape
+//                     </Text>
+//                 </Pressable>
+
+//                 <Pressable
+//                     onPress={lockPortrait}
+//                     style={{
+//                         flex: 1,
+//                         backgroundColor: "#FF6584",
+//                         padding: 12,
+//                         borderRadius: 8,
+//                         alignItems: "center",
+//                     }}
+//                 >
+//                     <Text
+//                         style={{
+//                             color: "white",
+//                         }}
+//                     >
+//                         Force Portrait
+//                     </Text>
+//                 </Pressable>
+//             </View>
+//         </SafeAreaView>
+//     );
+// };
+
+// export default HomeScreen;
+
+// const styles = StyleSheet.create({});
+
+import React from "react";
+import { StyleSheet, Text, View, useColorScheme } from "react-native";
+
+const themes = {
+    light: {
+        background: "#FFFFFF",
+        card: "#F5F5F5",
+        text: "1A1A1A",
+        subtext: "#666666",
+        accent: "#6C63FF",
+    },
+    dark: {
+        background: "#121212",
+        card: "#1E1E1E",
+        text: "FFFFFF",
+        subtext: "#AAAAAA",
+        accent: "#9D97FF",
+    },
+};
+
+const HomeScreen = () => {
+    const systemScheme = useColorScheme();
+    const 
+
+    return (
+        <View>
+            <Text>HomeScreen</Text>
+        </View>
+    );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
