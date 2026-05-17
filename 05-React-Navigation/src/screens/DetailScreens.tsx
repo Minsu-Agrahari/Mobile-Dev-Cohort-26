34f7782473bfa96afaeed9e9f54b28946f6707b1
Minsu-Agrahari <cmminsuagrahari@gmail.com>
2026-05-17 10:10:53 +0530

    [React Navigation] implemented <<navigation.goBack()>> in DetailScreen

--- 05-React-Navigation/src/screens/DetailScreens.tsx
+++ 05-React-Navigation/src/screens/DetailScreens.tsx
@@ -1,12 +1,16 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 import { Button } from "@react-navigation/elements";
 
 const DetailScreens = () => {
+  // Hook
+  const navigation = useNavigation<any>();
+
   return (
     <View>
       <Text>DetailScreens</Text>
-      <Button screen={"HomeScreen"}>Go To HomeScreen</Button>
+      <Button onPress={()=>navigation.goBack()}>Go Back z</Button>
     </View>
   );
 };
--- 05-React-Navigation/src/screens/HomeScreen.tsx
+++ 05-React-Navigation/src/screens/HomeScreen.tsx
@@ -1,12 +1,22 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Button } from "@react-navigation/elements";
+import { Link, useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
+  const navigation = useNavigation<any>();
+
   return (
     <View>
       <Text>HomeScreen</Text>
+
       <Button screen={"Details"}>Go To Details Screen</Button>
+
+      <Link screen={"Details"}>Go To Detail → Link</Link>
+
+      <Button onPress={() => navigation.navigate("Details")}>
+        Go To Detail Page via (Navigation)
+      </Button>
     </View>
   );
 };
